853b2dc0cc90a3d850c8e37630a99f6485158ec1
Marina <marinaradusheva9@gmail.com>
2022-05-12 15:31:55 +0300

    fixed issue with exposed key

---
+++ Web/GRHelper.Web/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿namespace GRHelper.Web.Hubs
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.SignalR;
+
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("NewMessage", message);
+        }
+    }
+}
